976b6cec676522e79db3fcc1f3c9fb0db60d0aee
Juan David Rengifo Castro <jdrengifoc@eafit.edu.co>
2024-06-10 21:49:02 -0500

    Upload untidy local-juan version.

---
+++ Code/Fixed/requirements.R
@@ -0,0 +1,11 @@
+packages <- c(
+  "doParallel", "snow", "fdrtool", "plm"
+)
+
+if (!all(packages %in% rownames(installed.packages()))){
+  idx <- which((packages %in% rownames(installed.packages()))==F)
+  install.packages(packages[idx])  
+}
+
+lapply(packages, library, character.only = TRUE) 
+rm(idx, packages)
